46f0086c99d56fa5e3f4a7f9349a4c2676a55a8c
herlan-jae <herlanjaelani1507@gmail.com>
2025-07-16 12:32:43 +0700

    finalizing

--- rescuego_api/settings.py
+++ rescuego_api/settings.py
@@ -154,10 +154,34 @@ LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 
-# CORS settings (jika diperlukan untuk API)
+# CORS settings
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+CORS_ALLOWED_METHOD = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'POST',
+    'PUT',
 ]
 
 # Django REST Framework settings
