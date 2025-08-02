869b7e4b470c940ab3e770587ad6de3637badb66
Herlan <72683838+herlan-jae@users.noreply.github.com>
2025-08-02 22:04:16 +0700

    Update settings.py

--- rescuego_api/settings.py
+++ rescuego_api/settings.py
@@ -168,6 +168,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5500",
     "http://127.0.0.1:5500",
+    "http://localhost:5501",
+    "http://127.0.0.1:5501",
+    "http://localhost:5502",
+    "http://127.0.0.1:5502",
 ]
 
 CORS_ALLOW_HEADERS = [
