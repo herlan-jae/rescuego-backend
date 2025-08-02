01f35ce4d429587340e290d117946a85fddf372d
Herlan <72683838+herlan-jae@users.noreply.github.com>
2025-08-02 21:49:06 +0700

    Update settings.py

--- rescuego_api/settings.py
+++ rescuego_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nu4x%&^qi*-zzd71&j&6b&xvg^ke_o8u8*+d(fdn-n%c2f-#(a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['herlanjae.pythonanywhere.com',
                 '127.0.0.1',
